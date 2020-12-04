0b879fabb9ae66333e862c7e368438498a813548
Ivan Savov <ivan.savov@gmail.com>
2020-12-04 12:12:13 -0500

    More hyperlins in serializers

--- standards/serializers.py
+++ standards/serializers.py
@@ -10,6 +10,37 @@ from standards.models import TERMREL_KINDS, TermRelation
 # CUTSOM HYPERLINK FIELDS
 ################################################################################
 
+
+class JurisdictionHyperlink(serializers.HyperlinkedRelatedField):
+    # /terms/<jurisdiction__name>
+    # We define these as class attributes so we don't need to pass them as args.
+    view_name = 'api-juri-detail'
+    queryset = Jurisdiction.objects.all()
+
+    def get_url(self, obj, view_name, request, format):
+        url_kwargs = {
+            'name': obj.name
+        }
+        if 'format' in request.GET:
+            # This is a hack to avoid ?format=api appended to URIs by preserve_builtin_query_params
+            # github.com/encode/django-rest-framework/blob/master/rest_framework/reverse.py#L12-L29
+            request.GET._mutable = True
+            del request.GET['format']
+            request.GET._mutable = False
+        return reverse(view_name, kwargs=url_kwargs, request=request)
+
+    def get_object(self, view_name, view_args, view_kwargs):
+        lookup_kwargs = {
+            'name': view_kwargs['name'],
+        }
+        return self.get_queryset().get(**lookup_kwargs)
+
+    def use_pk_only_optimization(self):
+        # via
+        # https://github.com/django-json-api/django-rest-framework-json-api/issues/489#issuecomment-428002360
+        return False
+
+
 class ControlledVocabularyHyperlink(serializers.HyperlinkedRelatedField):
     # /terms/<jurisdiction__name>/<name>
     # We define these as class attributes so we don't need to pass them as args.
@@ -36,6 +67,10 @@ class ControlledVocabularyHyperlink(serializers.HyperlinkedRelatedField):
         }
         return self.get_queryset().get(**lookup_kwargs)
 
+    def use_pk_only_optimization(self):
+        # via
+        # https://github.com/django-json-api/django-rest-framework-json-api/issues/489#issuecomment-428002360
+        return False
 
 class TermHyperlink(serializers.HyperlinkedRelatedField):
     # /terms/<jurisdiction__name>/<name>
@@ -71,19 +106,25 @@ class TermHyperlink(serializers.HyperlinkedRelatedField):
 ################################################################################
 
 class JurisdictionSerializer(serializers.ModelSerializer):
-    vocabularies = ControlledVocabularyHyperlink(many=True)
+    vocabularies = ControlledVocabularyHyperlink(many=True, required=False)
 
     class Meta:
         model = Jurisdiction
         fields = [
-            "id", "name", "display_name", "country", "language", "alt_name", "notes",
+            "id",
+            "name",
+            "display_name",
+            "country",
+            "language",
+            "alt_name",
+            "notes",
             "vocabularies",
         ]
 
 
 
 class ControlledVocabularySerializer(serializers.ModelSerializer):
-    terms = TermHyperlink(many=True)
+    terms = TermHyperlink(many=True, required=False)
 
     class Meta:
         model = ControlledVocabulary
@@ -108,7 +149,26 @@ class ControlledVocabularySerializer(serializers.ModelSerializer):
 
 
 class TermSerializer(serializers.ModelSerializer):
+    jurisdiction = JurisdictionHyperlink(source='vocabulary.jurisdiction', required=True)
+    vocabulary = ControlledVocabularyHyperlink(required=True)
+
     class Meta:
         model = Term
-        fields = '__all__'
+        fields = [
+            "jurisdiction",
+            "vocabulary",
+            "id",
+            "path",
+            "label",
+            "alt_label",
+            "hidden_label",
+            "notation",
+            "definition",
+            "notes",
+            "language",
+            "sort_order",
+            "date_created",
+            "date_modified",
+            "extra_fields",
+        ]
 
